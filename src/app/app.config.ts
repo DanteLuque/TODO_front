1a1e892aad7a834ea0e89a3d8c716617003aec5c
DanDev1004 <dandeveloper1004@gmail.com>
2025-10-02 23:55:33 -0500

    feat: 🎸 project base

---
+++ src/app/app.config.ts
@@ -0,0 +1,23 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+
+    providePrimeNG({
+      theme: {
+        preset:Aura
+      }
+    }),
+
+    provideAnimations(),
+  ]
+};
